047691ddf24d40d6e94996bb883dfafa891f325c
LatropAgency <vovakolesnikovich@gmail.com>
2021-01-18 23:15:30 +0300

    Optimized

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import logging
 import argparse
 import sys
+import time
 from contextlib import contextmanager
 from logging import handlers
 
+import bs4
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException, WebDriverException
 from selenium.webdriver.common.action_chains import ActionChains
@@ -19,17 +21,19 @@ import dateparser
 from datetime import datetime
 
 CSS_SELECTORS = {
+    'POST': 'div.Post',
     'VOTE_COUNT': '._1rZYMD_4xY3gRcSS3p8ODO',
     'COMMENT_COUNT': 'span.FHCV02u6Cp2zYL0fhQPsO',
     'CAKE_DAY': 'span#profile--id-card--highlight-tooltip--cakeday',
     'USER_KARMA': 'span#profile--id-card--highlight-tooltip--karma',
-    'CATEGORY': 'span._19bCWnxeTjqzBElWZfIlJb',
+    'CATEGORY': 'div._2mHuuvyV9doV3zwbZPtIPG > a._3ryJoIoycVkA88fy40qNJc',
     'POST_DATE': 'div._2J_zB4R1FH2EjGMkQjedwc',
     'POST_HOVER_DATE': 'a._3jOxDPIQ0KaOWpzvSQo-1s',
     'POST_LINK': 'a._3jOxDPIQ0KaOWpzvSQo-1s',
     'USER': 'a._2tbHP6ZydRpjI44J3syuqC._23wugcdiaj44hdfugIAlnX.oQctV4n0yUb0uiHDdGnmE',
-    'USER_CARD': 'div._m7PpFuKATP9fZF4xKf9R',
+    'USER_CARD': 'div._3uK2I0hi3JFTKnMUFHD2Pd',
     'CARD_KARMA': 'div._18aX_pAQub_mu1suz4-i8j',
+    'USERNAME': 'h1._3LM4tRaExed4x1wBfK1pmg',
 }
 
 LOGMODES = {
@@ -43,13 +47,12 @@ LOGMODES = {
 def get_element_text(driver, css_selector):
     return WebDriverWait(driver, 10) \
         .until(ec.presence_of_element_located((By.CSS_SELECTOR, css_selector))) \
-        .get_attribute('innerHTML')
+        .get_attribute('innerText')
 
 
 def show_element(driver, css_selector):
-    element = driver.find_element_by_css_selector(css_selector)
+    element = WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.CSS_SELECTOR, css_selector)))
     ActionChains(driver).move_to_element(element).perform()
-    return element
 
 
 def post_count_validator(arg):
@@ -128,7 +131,7 @@ def init_driver():
     options.add_argument("start-maximized")
     options.add_argument("disable-infobars")
     options.add_argument("--disable-extensions")
-    options.add_argument("--headless")
+    # options.add_argument("--headless")
     driver = webdriver.Chrome(options=options)
     logging.debug('WebDriver is initialized')
     return driver
@@ -140,38 +143,32 @@ def save(parsed_posts):
         logging.info(f'Successful saved to {file.name}')
 
 
-def parse_post(driver, url, parsed_posts):
-    logging.info(f'Url of post: {url}')
+def parse_post(post, parsed_posts):
     parsed_post = {}
     parsed_post['unique_id'] = str(uuid.uuid1())
-    parsed_post['url'] = url
-
-    with open_tab(driver, url):
-        get_post_info(driver, parsed_post)
-
-        if get_user_info(driver, parsed_post):
-            parsed_posts.append(parsed_post)
-            logging.info(f'Successfully parse: {len(parsed_posts)}')
+    parsed_post['url'] = post.find_element_by_css_selector(CSS_SELECTORS['POST_LINK']).get_attribute('href')
+    user_url = post.find_element_by_css_selector(CSS_SELECTORS['USER']).get_attribute('href')
+    if get_user_info(driver, parsed_post, user_url):
+        parsed_posts.append(parsed_post)
+        get_post_info(post, parsed_post)
+        logging.info(f'Successfully parse: {len(parsed_posts)}')
 
 
-def parse(driver, parsed_posts, count):
+def lookup(driver, count):
     logging.info(f'Parse is starting with count: {count}')
     driver.get('https://www.reddit.com/top/?t=month')
     index = 0
-
+    parsed_posts = []
     while len(parsed_posts) < count:
-        links = driver.find_elements_by_css_selector(CSS_SELECTORS['POST_LINK'])
-        links = links[index:len(links)]
+        posts = driver.find_elements_by_css_selector(CSS_SELECTORS['POST'])
+        posts = posts[index:len(posts)]
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-        for link in links:
-            parse_post(driver, link.get_attribute('href'), parsed_posts)
+        for post in posts:
+            parse_post(post, parsed_posts)
             index += 1
-
             if (len(parsed_posts)) == count:
                 break
-
-    logging.info(f'{count} posts successfully processed')
+    return parsed_posts
 
 
 def get_post_date(driver):
@@ -179,30 +176,26 @@ def get_post_date(driver):
     return dateparser.parse(post_date).strftime("%Y-%m-%d")
 
 
-def get_post_info(driver, parsed_post):
-    parsed_post['post_date'] = get_post_date(driver)
-    parsed_post['comment_count'] = get_element_text(driver, CSS_SELECTORS['COMMENT_COUNT']).split(' ')[0]
-    parsed_post['vote_count'] = get_element_text(driver, CSS_SELECTORS['VOTE_COUNT'])
-    parsed_post['category'] = get_element_text(driver, CSS_SELECTORS['CATEGORY']).split('/')[1]
+def get_post_info(post, parsed_post):
+    parsed_post['post_date'] = get_post_date(post)
+    parsed_post['comment_count'] = get_element_text(post, CSS_SELECTORS['COMMENT_COUNT']).split(' ')[0]
+    parsed_post['vote_count'] = get_element_text(post, CSS_SELECTORS['VOTE_COUNT'])
+    parsed_post['category'] = get_element_text(post, CSS_SELECTORS['CATEGORY']).split('/')[1]
 
 
-def get_user_info(driver, parsed_post):
-    wait = WebDriverWait(driver, 10)
-    parsed_post['username'] = get_element_text(driver, CSS_SELECTORS['USER']).split('/')[1]
-    user_element = show_element(driver, CSS_SELECTORS['USER'])
-    wait.until(ec.presence_of_element_located((By.CSS_SELECTOR, CSS_SELECTORS['USER_CARD'])))
-    card_element = driver.find_elements_by_css_selector(CSS_SELECTORS['CARD_KARMA'])
-    parsed_post['post_karma'] = card_element[0].get_attribute('innerHTML')
-    parsed_post['comment_karma'] = card_element[1].get_attribute('innerHTML')
-    user_element.click()
-    try:
-        logging.info(f'User: {parsed_post["username"]} is valid')
-        parsed_post['user_karma'] = get_element_text(driver, CSS_SELECTORS['USER_KARMA'])
-        parsed_post['cake_day'] = get_element_text(driver, CSS_SELECTORS['CAKE_DAY'])
-        return True
-    except TimeoutException:
-        logging.info(f'User: {parsed_post["username"]} is not valid')
-        return False
+def get_user_info(driver, parsed_post, user_url):
+    with open_tab(driver, user_url):
+        try:
+            parsed_post['username'] = user_url.split('/')[-2]
+            parsed_post['user_karma'] = get_element_text(driver, CSS_SELECTORS['USER_KARMA'])
+            parsed_post['cake_day'] = get_element_text(driver, CSS_SELECTORS['CAKE_DAY'])
+            show_element(driver, CSS_SELECTORS['USER_KARMA'])
+            karma = get_element_text(driver, CSS_SELECTORS['USER_CARD']).split('\n')
+            parsed_post['post_karma'] = karma[0].split(' ')[0]
+            parsed_post['comment_karma'] = karma[1].split(' ')[0]
+            return True
+        except TimeoutException:
+            return False
 
 
 if __name__ == '__main__':
@@ -213,11 +206,10 @@ if __name__ == '__main__':
     parser.add_argument('--logmode', required=False, default='ALL', type=logmode_validator,
                         help='Log mode  - ALL - all levers, ERROR - only ERROR lever, WARNING - only WARNING lever, DISABLE - no console console log')
     args = parser.parse_args()
-    parsed_posts = []
     init_logger(args.logmode)
     try:
         with open_webdriver() as driver:
-            parse(driver, parsed_posts, args.count)
+            parsed_posts = lookup(driver, args.count)
             save(parsed_posts)
             logging.debug(f'The duration of the scraping: {datetime.now() - start}')
     except WebDriverException as e:
