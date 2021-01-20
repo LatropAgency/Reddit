623648d840965809a852e1bf7c740874f982bc88
LatropAgency <vovakolesnikovich@gmail.com>
2021-01-20 17:49:58 +0300

    Added Storage

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
+import argparse
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from datetime import datetime
 import re
 
+from validators import unsigned_int_validator
+
 HOSTNAME = 'localhost'
-PORT = 8087
 KEYS = ['unique_id',
         'url',
         'username',
@@ -19,37 +21,47 @@ KEYS = ['unique_id',
         ]
 
 
-class Server(BaseHTTPRequestHandler):
+class Storage:
+    def __init__(self, keys):
+        self.keys = keys
+        self.items = self.get_all()
+
+    def get_all(self):
+        with open(f'{datetime.today().strftime("reddit-%Y%m%d")}.txt', 'r') as f:
+            items = [line.split(';') for line in f.read().split('\n') if line != '']
+        return {item[0]: dict(zip(self.keys, item)) for item in items}
+
+    def get_by_id(self, id):
+        return self.items.get(id, None)
+
+    def insert(self, item):
+        self.items[item['unique_id']] = item
+
+    def delete(self, id):
+        del self.items[id]
+
+    def update(self, id, item):
+        for key, value in item.items():
+            self.items[id][key] = value
+
+
+class HttpHandler(BaseHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        self.storage = Storage(KEYS)
+        super(BaseHTTPRequestHandler, self).__init__(*args, **kwargs)
+
     def _set_headers(self, status_code):
         self.send_response(status_code)
         self.send_header("Content-type", "application/json")
         self.end_headers()
 
-    def _get_all_posts(self):
-        with open(f'{datetime.today().strftime("reddit-%Y%m%d")}.txt', 'r') as f:
-            posts = [line.split(';') for line in f.read().split('\n') if line != '']
-        return {post[0]: dict(zip(KEYS, post)) for post in posts}
-
-    def _get_post(self, unique_id):
-        posts = self._get_all_posts()
-        return posts.get(unique_id, None)
-
-    def _save(self, posts):
-        with open(f'{datetime.today().strftime("reddit-%Y%m%d")}.txt', "w") as file:
-            file.writelines([';'.join(post.values()) + '\n' for post in posts.values()])
-
-    def _append(self, post):
-        with open(f'{datetime.today().strftime("reddit-%Y%m%d")}.txt', 'a+') as f:
-            f.write(';'.join(post.values()) + '\n')
-
     def do_GET(self):
-        if (self.path == '/posts/'):
-            posts = self._get_all_posts()
+        if self.path == '/posts/':
             self._set_headers(200)
-            self.wfile.write(bytes(json.dumps(list(posts.values())), 'utf-8'))
+            self.wfile.write(bytes(json.dumps(list(self.storage.items.values())), 'utf-8'))
         elif re.match(r'/posts/[a-z0-9-]{36}/', self.path):
             unique_id = self.path.split('/')[2]
-            post = self._get_post(unique_id)
+            post = self.storage.get_by_id(unique_id)
             if post:
                 self._set_headers(200)
                 self.wfile.write(bytes(json.dumps(post), 'utf-8'))
@@ -59,45 +71,48 @@ class Server(BaseHTTPRequestHandler):
             self._set_headers(200)
 
     def do_POST(self):
-        if (self.path == '/posts/'):
+        if self.path != '/posts/':
+            self._set_headers(200)
+        else:
             length = int(self.headers.get('Content-Length'))
             body = self.rfile.read(length).decode('utf-8')
             post = json.loads(body)
-            print(post)
-            self._append(post)
+            self.storage.insert(post)
             self._set_headers(201)
-            self.wfile.write(bytes(json.dumps(post), 'utf-8'))
-        else:
-            self._set_headers(200)
+            self.wfile.write(
+                bytes(json.dumps({post['unique_id']: list(self.storage.items.keys()).index(post['unique_id'])}),
+                      'utf-8'))
 
     def do_DELETE(self):
-        posts = self._get_all_posts()
-        if re.match(r'/posts/[a-z0-9-]{36}/', self.path):
-            unique_id = self.path.split('/')[2]
-            del posts[unique_id]
-            self._set_headers(200)
-            self._save(posts)
-        else:
+        if not re.match(r'/posts/[a-z0-9-]{36}/', self.path):
             self._set_headers(404)
+        else:
+            unique_id = self.path.split('/')[2]
+            if unique_id in self.storage.items.keys():
+                self._set_headers(200)
+                self.storage.delete(unique_id)
+            else:
+                self._set_headers(404)
 
     def do_PUT(self):
-        posts = self._get_all_posts()
-        if re.match(r'/posts/[a-z0-9-]{36}/', self.path):
+        if not re.match(r'/posts/[a-z0-9-]{36}/', self.path):
+            self._set_headers(404)
+        else:
             unique_id = self.path.split('/')[2]
             length = int(self.headers.get('Content-Length'))
             body = self.rfile.read(length).decode('utf-8')
             post = json.loads(body)
-            for key, value in post.items():
-                posts[unique_id][key] = value
+            self.storage.update(unique_id, post)
             self._set_headers(200)
-            self._save(posts)
-        else:
-            self._set_headers(404)
 
 
 if __name__ == "__main__":
-    webServer = HTTPServer((HOSTNAME, PORT), Server)
-    print("Server started http://%s:%s" % (HOSTNAME, PORT))
+    parser = argparse.ArgumentParser(description='Reddit parser')
+    parser.add_argument('port', type=unsigned_int_validator, help='Port of HTTP server')
+    args = parser.parse_args()
+
+    webServer = HTTPServer((HOSTNAME, args.port), HttpHandler)
+    print("Server started http://%s:%s" % (HOSTNAME, args.port))
 
     try:
         webServer.serve_forever()
