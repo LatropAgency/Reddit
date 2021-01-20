74020719c319df05f86f31a51f371ea7bed06dca
LatropAgency <vovakolesnikovich@gmail.com>
2021-01-20 19:42:47 +0300

    Added router

--- server.py
+++ server.py
@@ -69,63 +69,89 @@ class HttpHandler(BaseHTTPRequestHandler):
     storage = None
 
     def __init__(self, *args, **kwargs):
+        self.router = {
+            'GET': {
+                r'^/posts/$': self.get_all_posts,
+                r'^/posts/[a-z0-9-]{36}/$': self.get_post,
+            },
+            'POST': {
+                r'^/posts/$': self.insert_post,
+            },
+            'DELETE': {
+                r'^/posts/[a-z0-9-]{36}/$': self.delete_post
+            },
+            'PUT': {
+                r'^/posts/[a-z0-9-]{36}/$': self.update_post
+            },
+            'DEFAULT': self.default_route,
+        }
         self.storage = self.__class__.storage
         super(BaseHTTPRequestHandler, self).__init__(*args, **kwargs)
 
-    def _set_headers(self, status_code):
+    def set_headers(self, status_code):
         self.send_response(status_code)
         self.send_header("Content-type", "application/json")
         self.end_headers()
 
-    def do_GET(self):
-        if self.path == '/posts/':
-            self._set_headers(200)
-            self.wfile.write(bytes(json.dumps(list(self.storage.items.values())), 'utf-8'))
-        elif re.match(r'/posts/[a-z0-9-]{36}/', self.path):
-            unique_id = self.path.split('/')[2]
-            post = self.storage.get_by_id(unique_id)
-            if post:
-                self._set_headers(200)
-                self.wfile.write(bytes(json.dumps(post), 'utf-8'))
-            else:
-                self._set_headers(404)
+    def default_route(self):
+        self.set_headers(200)
+
+    def get_all_posts(self):
+        self.set_headers(200)
+        self.wfile.write(bytes(json.dumps(list(self.storage.items.values())), 'utf-8'))
+
+    def insert_post(self):
+        length = int(self.headers.get('Content-Length'))
+        body = self.rfile.read(length).decode('utf-8')
+        post = json.loads(body)
+        self.storage.insert(post, post['unique_id'])
+        self.set_headers(201)
+        self.wfile.write(
+            bytes(json.dumps({post['unique_id']: list(self.storage.items.keys()).index(post['unique_id'])}),
+                  'utf-8'))
+
+    def delete_post(self):
+        unique_id = self.path.split('/')[2]
+        if unique_id in self.storage.items.keys():
+            self.set_headers(200)
+            self.storage.delete(unique_id)
         else:
-            self._set_headers(200)
+            self.set_headers(404)
+
+    def get_post(self):
+        unique_id = self.path.split('/')[2]
+        post = self.storage.get_by_id(unique_id)
+        if post:
+            self.set_headers(200)
+            self.wfile.write(bytes(json.dumps(post), 'utf-8'))
+        else:
+            self.set_headers(404)
+
+    def update_post(self):
+        unique_id = self.path.split('/')[2]
+        length = int(self.headers.get('Content-Length'))
+        body = self.rfile.read(length).decode('utf-8')
+        post = json.loads(body)
+        self.storage.update(unique_id, post)
+        self.set_headers(200)
+
+    def find_route(self, method):
+        for route in self.router[method].keys():
+            if re.match(route, self.path):
+                return self.router[method][route]()
+        return self.router['DEFAULT']()
+
+    def do_GET(self):
+        self.find_route('GET')
 
     def do_POST(self):
-        if self.path != '/posts/':
-            self._set_headers(200)
-        else:
-            length = int(self.headers.get('Content-Length'))
-            body = self.rfile.read(length).decode('utf-8')
-            post = json.loads(body)
-            self.storage.insert(post, post['unique_id'])
-            self._set_headers(201)
-            self.wfile.write(
-                bytes(json.dumps({post['unique_id']: list(self.storage.items.keys()).index(post['unique_id'])}),
-                      'utf-8'))
+        self.find_route('POST')
 
     def do_DELETE(self):
-        if not re.match(r'/posts/[a-z0-9-]{36}/', self.path):
-            self._set_headers(404)
-        else:
-            unique_id = self.path.split('/')[2]
-            if unique_id in self.storage.items.keys():
-                self._set_headers(200)
-                self.storage.delete(unique_id)
-            else:
-                self._set_headers(404)
+        self.find_route('DELETE')
 
     def do_PUT(self):
-        if not re.match(r'/posts/[a-z0-9-]{36}/', self.path):
-            self._set_headers(404)
-        else:
-            unique_id = self.path.split('/')[2]
-            length = int(self.headers.get('Content-Length'))
-            body = self.rfile.read(length).decode('utf-8')
-            post = json.loads(body)
-            self.storage.update(unique_id, post)
-            self._set_headers(200)
+        self.find_route('PUT')
 
 
 if __name__ == "__main__":
